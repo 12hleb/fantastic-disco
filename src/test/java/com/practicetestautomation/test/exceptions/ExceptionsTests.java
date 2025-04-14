60827a1261dbe6a9b327233623716c9fbd477cbb
12hleb <12hleb@gmail.com>
2025-04-14 18:31:04 -0400

    Add implicitlyWait to handle the exception

--- src/test/java/com/practicetestautomation/test/exceptions/ExceptionsTests.java
+++ src/test/java/com/practicetestautomation/test/exceptions/ExceptionsTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,7 +37,7 @@ public class ExceptionsTests {
                 driver = new ChromeDriver();
                 break;
         }
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/");
     }
